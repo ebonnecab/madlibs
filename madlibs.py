24e4d7e70f7ea1b994333b538d4e75ce796ad06c
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-10-28 18:42:08 -0700

    finishing v0 of madlibs

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,7 @@
-noun1 = input("Input a noun and click enter")
-verb1 = input("Input a verb and click enter")
-place1 = input("Input a place and click enter")
-adj1 = input("Input an adjective and click enter")
+noun1 = input("Input a noun and click enter: ")
+verb1 = input("Input a verb and click enter: ")
+place1 = input("Input a place and click enter: ")
+adj1 = input("Input an adjective and click enter: ")
 
-print(noun1, verb1, place1, adj1)
+print('\nOnce upon a time, a small {}\n came along and {}\n my socks. I walked to {}\n and bought a {}\n pair'.format(noun1, verb1, place1, adj1))
+
