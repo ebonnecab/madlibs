d4733bba581f6a8f8efb6a0d79bb4e98b33874d9
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-10-28 18:21:53 -0700

    init

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+noun1 = input("Input a noun and click enter")
+verb1 = input("Input a verb and click enter")
+place1 = input("Input a place and click enter")
+adj1 = input("Input an adjective and click enter")
+
+print(noun1, verb1, place1, adj1)
